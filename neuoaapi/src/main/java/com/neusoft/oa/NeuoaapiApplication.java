88047e9ac2e27f1efd55368b573ad38f6a331f37
吕海东 <haidonglu@126.com>
2020-07-21 21:05:49 +0800

    第1次提交

---
+++ neuoaapi/src/main/java/com/neusoft/oa/NeuoaapiApplication.java
@@ -0,0 +1,13 @@
+package com.neusoft.oa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuoaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuoaapiApplication.class, args);
+	}
+
+}
