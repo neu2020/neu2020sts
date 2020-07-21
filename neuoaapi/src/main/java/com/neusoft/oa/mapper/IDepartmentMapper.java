0fcf13ab3200ec77adee0d4cd739a9ae138cdad3
吕海东 <haidonglu@126.com>
2020-07-21 22:01:22 +0800

    增加mybatis配置

---
+++ neuoaapi/src/main/java/com/neusoft/oa/mapper/IDepartmentMapper.java
@@ -0,0 +1,8 @@
+package com.neusoft.oa.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface IDepartmentMapper {
+
+}
---
+++ neuoaapi/src/main/java/com/neusoft/oa/model/DepartmentModel.java
@@ -0,0 +1,10 @@
+package com.neusoft.oa.model;
+
+import java.io.Serializable;
+
+import org.apache.ibatis.type.Alias;
+
+@Alias("Department")
+public class DepartmentModel implements Serializable {
+
+}
